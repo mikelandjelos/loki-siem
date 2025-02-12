5d1a6061899822982020a078804b02b7183efb26
mikelandjelos <mihajlo.madic@gmail.com>
2025-02-12 21:36:53 +0100

    Parsing refactoring - starting to write benchmark configurations

--- pipelines/pipelines/parsing/drain/configs/apache_loghub.py
+++ pipelines/pipelines/parsing/drain/configs/apache_loghub.py
@@ -2,9 +2,9 @@ from os import path
 
 from .common import COMMON_PATTERNS, RESULTS_ROOT_DIR, DrainConfig
 
-APACHE_2K_CONFIG: DrainConfig = DrainConfig(
+APACHE_LOGHUB: DrainConfig = DrainConfig(
     log_format=r"\[<Weekday> <Month> <Day> <Time> <Year>\] \[<Level>\] <Content>",
-    indir="data/loghub/Apache",
+    indir="data/loghub_full/Apache",
     outdir=path.join(RESULTS_ROOT_DIR, "drain/"),
     rex=COMMON_PATTERNS,
     st=0.4,  # Similarity threshold - greater ST, greater number of templates
--- pipelines/pipelines/parsing/drain/configs/configs.py
+++ pipelines/pipelines/parsing/drain/configs/configs.py
@@ -1,9 +1,9 @@
-from .apache_2k_config import APACHE_2K_CONFIG
 from .apache_elfak_config import APACHE_ELFAK_CONFIG
+from .apache_loghub import APACHE_LOGHUB
 
 # <semantic_name>: (<config_object>, <log_file>)
 
 CONFIGS = {
-    "apache_2k": (APACHE_2K_CONFIG, "Apache_2k.log"),
+    "apache_loghub": (APACHE_LOGHUB, "Apache_full.log"),
     "apache_elfak": (APACHE_ELFAK_CONFIG, "Access Logs-data-2024-10-11 10_22_32.log"),
 }
